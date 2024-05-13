60ed031ad60313e401495e65f43705ba727ce134
Gulbeyaz <glbzbayram@gmail.com>
2024-05-13 12:01:34 +0300

    FlatList was added to list of courses

--- myCourses/app/(tabs)/index.tsx
+++ myCourses/app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import {
   Button,
+  FlatList,
   StatusBar,
   StyleSheet,
   Text,
@@ -11,6 +12,7 @@ import AddCourseModal from "../../components/AddCourseModal";
 
 export default function HomeScreen() {
   const [isModalOpen, setModalOpen] = useState(false);
+  const [courses, setCourses] = useState([]);
 
   const openModal = () => {
     setModalOpen(true);
@@ -19,10 +21,29 @@ export default function HomeScreen() {
     <>
       <StatusBar barStyle="light-content" />
       <View style={styles.container}>
+        {isModalOpen && (
+          <AddCourseModal
+            modalVisible={isModalOpen}
+            setModalVisible={setModalOpen}
+            setCourses={setCourses}
+            courses={courses}
+          />
+        )}
+        <FlatList
+          data={courses}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.coursesContainer}>
+                <TouchableOpacity>
+                  <Text style={styles.courseList}>{item}</Text>
+                </TouchableOpacity>
+              </View>
+            );
+          }}
+        />
         <TouchableOpacity style={styles.addCourseBtn} onPress={openModal}>
           <Text style={styles.addCourseText}>Kurs Ekle</Text>
         </TouchableOpacity>
-        {isModalOpen && <AddCourseModal />}
       </View>
     </>
   );
@@ -32,16 +53,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 20,
+    paddingVertical: 40,
   },
   addCourseBtn: {
     backgroundColor: "red",
-    padding: 20,
-    borderRadius: 15,
+    padding: 12,
+    borderRadius: 20,
   },
   addCourseText: {
     color: "white",
     textAlign: "center",
     fontSize: 20,
     fontWeight: "bold",
+  },
+  coursesContainer: {
+    backgroundColor: "blue",
+    marginVertical: 5,
+    padding: 10,
+    borderRadius: 20,
+  },
+  courseList: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
   },
 });
