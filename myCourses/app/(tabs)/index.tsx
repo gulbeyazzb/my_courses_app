095fa6c2562ff3754a8af134fcf4b3b88e6dbe20
Gulbeyaz <glbzbayram@gmail.com>
2024-05-13 10:31:53 +0300

    Button was added to adding course by TouchableOpacity

--- myCourses/app/(tabs)/index.tsx
+++ myCourses/app/(tabs)/index.tsx
@@ -1,7 +1,47 @@
-import { StyleSheet } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import AddCourseModal from "../../components/AddCourseModal";
 
 export default function HomeScreen() {
-  return;
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.addCourseBtn} onPress={openModal}>
+          <Text style={styles.addCourseText}>Kurs Ekle</Text>
+        </TouchableOpacity>
+        {isModalOpen && <AddCourseModal />}
+      </View>
+    </>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  addCourseBtn: {
+    backgroundColor: "red",
+    padding: 20,
+    borderRadius: 15,
+  },
+  addCourseText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
